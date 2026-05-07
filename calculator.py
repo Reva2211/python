d94e6ea8509d539be113c91d2e3b3e9f221a5f64
reva2211 <revathijk2211@gmail.com>
2026-05-07 20:41:15 +0530

    calculator.py added

---
+++ calculator.py
@@ -0,0 +1,11 @@
+num1=float(input("enter the first number"))
+num2=float(input("enter the second number"))
+op=input("enter operator:")
+if op== "+":
+   print(num1+num2)
+elif op== "-":
+    print(num1-num2)
+elif op== "*":
+    print(num1*num2)
+else:
+    print(num1/num2)
